b84bb0f2597fe9d4c9be5181f5f095ec9b0e223b
Liukaku <46352428+Liukaku@users.noreply.github.com>
2022-06-22 12:55:17 +0100

    add interval for stock check

--- components/ProductDetails.tsx
+++ components/ProductDetails.tsx
@@ -7,9 +7,11 @@ const ProductDetails = ({ blok }: any) => {
   const [selected, updateSelected] = useState<number>(0);
   const [size, selectSize] = useState<number | null>(null);
   const [basketErr, updateErr] = useState<string>("");
+  const [theInterval, setTheInt] = useState<any>(null);
 
-  useEffect(() => {
+  const getData = () => {
     const urlArr = document.URL.split("/");
+
     console.log(urlArr[urlArr.length - 1]);
     const productId = { productID: urlArr[urlArr.length - 1] };
     let fetchURL = "";
@@ -26,11 +28,19 @@ const ProductDetails = ({ blok }: any) => {
         return res.json();
       })
       .then((data) => {
+        console.log("beep");
         updateProduct(data.products);
+        setInterval;
       })
       .catch((err) => {
         console.log(err);
       });
+  };
+
+  useEffect(() => {
+    getData();
+    setTheInt(setInterval(() => getData(), 60000));
+    return clearInterval(theInterval);
   }, []);
 
   useEffect(() => {
@@ -44,7 +54,7 @@ const ProductDetails = ({ blok }: any) => {
   };
 
   return product ? (
-    <div className="w-8/12  mt-5 h-full mx-auto pb-5">
+    <div className="w-9/12  mt-16 h-full mx-auto pb-5">
       <h1 className="text-sm robotoMedium">
         <span className="underline text-blue-600">
           <Link href={"/"}>Home</Link>
@@ -52,31 +62,50 @@ const ProductDetails = ({ blok }: any) => {
         {">"} {product.gender} {">"} {product.title} {product.gender}s Shoes
       </h1>
       <div className="w-full bg-zinc-100 pb-2">
-        <div className="w-full h-full flex justify-between px-2">
-          <div className=" w-32 flex flex-wrap justify-end">
-            {product.imageURL.map((url: string, n: number) => {
-              return (
-                <div
-                  onClick={() => {
-                    updateSelected(n);
-                  }}
-                  className={`h-28 w-full bg-cover border-2 my-3 cursor-pointer border-zinc-100  ${
-                    selected === n ? `border-b-zinc-300` : ``
-                  }`}
-                  style={{ backgroundImage: `url(${url})` }}
-                ></div>
-              );
-            })}
+        <div className="w-full h-full flex md:flex-none flex-wrap justify-between px-2">
+          <div className="lg:flex lg:w-[66%] w-full">
+            <div className="w-32 lg:flex hidden flex-wrap justify-end">
+              {product.imageURL.map((url: string, n: number) => {
+                return (
+                  <div
+                    onClick={() => {
+                      updateSelected(n);
+                    }}
+                    className={`h-28 w-full bg-cover border-2 my-3 cursor-pointer border-zinc-100  ${
+                      selected === n ? `border-b-zinc-300` : ``
+                    }`}
+                    style={{ backgroundImage: `url(${url})` }}
+                  ></div>
+                );
+              })}
+            </div>
+            <div className="mx-auto my-auto lg:h-[30vw] lg:w-[30vw] w-[60vw] h-[60vw] inherit">
+              <div className=" h-full w-full border mx-auto mt-3 border-zinc-100  relative">
+                <Image
+                  layout="fill"
+                  className="bg-zinc-100"
+                  src={product.imageURL[selected]}
+                />
+                {/* style={{ backgroundImage: `url(${product.imageURL[selected]})` }} */}
+              </div>
+            </div>
+            <div className="w-full lg:hidden flex flex-wrap">
+              {product.imageURL.map((url: string, n: number) => {
+                return (
+                  <div
+                    onClick={() => {
+                      updateSelected(n);
+                    }}
+                    className={`h-28 w-2/12 bg-cover border-2 my-3 cursor-pointer border-zinc-100  ${
+                      selected === n ? `border-b-zinc-300` : ``
+                    }`}
+                    style={{ backgroundImage: `url(${url})` }}
+                  ></div>
+                );
+              })}
+            </div>
           </div>
-          <div className=" h-productImg border ml-3 mt-3 border-zinc-100 w-productImg relative">
-            <Image
-              layout="fill"
-              className="bg-zinc-100"
-              src={product.imageURL[selected]}
-            />
-            {/* style={{ backgroundImage: `url(${product.imageURL[selected]})` }} */}
-          </div>
-          <div className="w-4/12 mt-3 ml-12 bg-white">
+          <div className="lg:w-4/12 w-full mt-3  ml-0 bg-white">
             <div className="px-5 mt-3">
               <h1 className="text-sm">{product.brand}</h1>
               <h1 className="text-4xl robotoBold">{product.title}</h1>
@@ -84,32 +113,34 @@ const ProductDetails = ({ blok }: any) => {
               <h1 className="text-sm">{product.gender}s shoes</h1>
             </div>
 
-            <div className="flex flex-wrap flex-start px-5">
+            <div className="px-5 lg:pb-0 pb-5">
               <hr className="border bg-zinc-200 border-zinc-200 w-full mx-1" />
-              {Object.keys(product.sizes).map((key: string, k: number) => {
-                if (product.sizes[key] === 0) {
-                  return (
-                    <div
-                      className={`text-center lg:w-20 md:w-16 robotoBold lg:mx-1 my-2 py-2 text-zinc-600 bg-zinc-100 outOfStock`}
-                    >
-                      <p className="w-full px-5 ">{key}</p>
-                    </div>
-                  );
-                } else {
-                  return (
-                    <div
-                      onClick={() => {
-                        selectSize(k);
-                      }}
-                      className={`text-center w-20 robotoBold mx-1 my-2 py-2 text-blackbg-zinc-300 cursor-pointer bg-zinc-200 hover:bg-zinc-600 hover:text-white ${
-                        size === k ? `text-white bg-zinc-600` : ""
-                      }`}
-                    >
-                      <p className="w-full px-5 ">{key}</p>
-                    </div>
-                  );
-                }
-              })}
+              <div className="w-11/12 flex lg:flex-start flex-wrap mx-auto">
+                {Object.keys(product.sizes).map((key: string, k: number) => {
+                  if (product.sizes[key] === 0) {
+                    return (
+                      <div
+                        className={`text-center lg:w-20 md w-2/12 robotoBold mx-1 my-2 py-2 text-zinc-600 bg-zinc-100 outOfStock`}
+                      >
+                        <p className="w-full px-5 ">{key}</p>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        onClick={() => {
+                          selectSize(k);
+                        }}
+                        className={`text-center lg:w-20 w-2/12 robotoBold mx-1 my-2 py-2 text-black cursor-pointer bg-zinc-200 hover:bg-zinc-600 hover:text-white ${
+                          size === k ? `text-white bg-zinc-600` : ""
+                        }`}
+                      >
+                        <p className="w-full px-5 ">{key}</p>
+                      </div>
+                    );
+                  }
+                })}
+              </div>
               {basketErr ? (
                 <span className="text-red-500 w-full">{basketErr}</span>
               ) : (
